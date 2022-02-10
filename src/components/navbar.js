610cc5f9339ab6bda9abadc740b37addcfc239a7
Splicid <luis1abreu11@gmail.com>
2022-02-10 05:35:49 -0500

    Starter

---
+++ src/components/navbar.js
@@ -0,0 +1,28 @@
+import '../styles/navbar.css'
+
+function navbar(){
+    return(
+        <div>
+            <ul className="nav-list">
+                <li className="nav-item">
+                <a href="">Logo</a>
+                </li>
+                <li className="nav-item">
+                <a href="">About</a>
+                </li>
+                <li className="nav-item">
+                <a href="">Work</a>
+                </li>
+                <li className="nav-item">
+                <a href="">Contact</a>
+                </li>
+                <li className="nav-item">
+                    <button className='btn-primary'> Donate </button>
+                </li>
+            </ul>
+        </div>
+
+    );
+}
+
+export default navbar;
